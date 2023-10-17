482f279bff01b2d797aa61946f4b2f09179468a4
Akshay Gopan <37625354+akshaygopan@users.noreply.github.com>
2023-10-17 07:18:28 -0400

    updated the third session

---
+++ src/screens/Home/ThirdSection/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./ThirdSection.css";
+
+import { MdAirlineSeatReclineExtra } from "react-icons/md";
+import { FaWifi } from "react-icons/fa";
+import { GiFirstAidKit } from "react-icons/gi";
+import { FaUserTie } from "react-icons/fa";
+
+const ThirdSection = () => {
+  return(
+  <div className="third-session">
+      <div className="content">
+         <span className="title">What we Offer</span>
+      </div>
+     <div className="features">
+         <div className="rows">
+                  <div className="box">
+                  <MdAirlineSeatReclineExtra />
+                  <div>Comfort Seats</div>
+             </div>
+             <div className="box">
+                  <FaWifi />
+                  <div>Wifi</div>
+             </div>
+         </div>
+          <div className="rows">
+             <div className="box">
+                  <GiFirstAidKit />
+                  <div>First Aid & Safety</div>
+             </div>
+             < div className="box">
+                  <FaUserTie/>
+                  <div>Friendly Drivers</div>
+             </div>
+         </div>
+     </div>
+  </div>
+  );
+}
+
+export default ThirdSection;
